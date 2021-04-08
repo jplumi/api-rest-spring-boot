de4667ded7e396660f4477d4eaeb21c49c5c16f9
jplumi <jpedro.lumi@gmail.com>
2021-04-08 09:32:20 -0300

    Iniciando o projeto, classe Product

---
+++ src/main/java/com/produtos/apiRest/models/Product.java
@@ -0,0 +1,50 @@
+package com.produtos.apiRest.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "TB_PRODUCT")
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    private Integer quantidade;
+    private BigDecimal valor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
